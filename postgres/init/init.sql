1c3405ab6b422ccaa9f96ac68a00916a3fa958eb
Илья <iliya97pro@yandex.ru>
2023-08-05 13:44:44 +0300

    update

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -5,7 +5,7 @@ CREATE SCHEMA shop;
 
 CREATE TABLE shop.company
 (
-    id    int PRIMARY KEY,
+    id    serial PRIMARY KEY,
     name  varchar(50) NOT NULL,
     phone varchar(50) NOT NULL,
     email varchar(50) NOT NULL,
@@ -14,50 +14,50 @@ CREATE TABLE shop.company
 
 CREATE TABLE shop.category
 (
-    id   int PRIMARY KEY,
+    id   serial PRIMARY KEY,
     name varchar(50) NOT NULL
 );
 
 CREATE TABLE shop.product
 (
-    id          int PRIMARY KEY,
+    id          serial PRIMARY KEY,
     name        varchar(50)            NOT NULL,
     price       decimal(10, 2)         NOT NULL CHECK ( price >= 0 ),
-    maker_id    int REFERENCES company NOT NULL,
-    provider_id int REFERENCES company NOT NULL,
-    category_id int REFERENCES category
+    maker_id    int REFERENCES shop.company NOT NULL,
+    provider_id int REFERENCES shop.company NOT NULL,
+    category_id int REFERENCES shop.category
 );
 
 CREATE TABLE shop.consumer
 (
-    id        int PRIMARY KEY,
+    id        serial PRIMARY KEY,
     full_name varchar(80) NOT NULL,
     phone     varchar(50) NOT NULL
 );
 
 CREATE TABLE shop.purchase
 (
-    id          bigint PRIMARY KEY,
+    id          bigserial PRIMARY KEY,
     date        date                    NOT NULL,
     sum         decimal(14, 2)          NOT NULL,
     address     varchar(50)             NOT NULL,
-    consumer_id int REFERENCES consumer NOT NULL
+    consumer_id int REFERENCES shop.consumer NOT NULL
 );
 
 CREATE TABLE shop.product_purchase
 (
-    product_id  int REFERENCES product     NOT NULL,
-    purchase_id bigint REFERENCES purchase NOT NULL,
+    product_id  serial REFERENCES shop.product     NOT NULL,
+    purchase_id bigint REFERENCES shop.purchase NOT NULL,
     PRIMARY KEY (product_id, purchase_id)
 );
 
-CREATE INDEX shop.idx_company_name ON company (name);
-CREATE INDEX shop.idx_product_name ON product (name);
-CREATE INDEX shop.idx_product_price ON product (price);
-CREATE INDEX shop.idx_category_name ON category (name);
-CREATE INDEX shop.idx_purchase_date ON purchase (date);
-CREATE INDEX shop.idx_purchase_sum ON purchase (sum);
-CREATE INDEX shop.idx_consumer_full_name ON consumer (full_name);
+CREATE INDEX shop.idx_company_name ON shop.company (name);
+CREATE INDEX shop.idx_product_name ON shop.product (name);
+CREATE INDEX shop.idx_product_price ON shop.product (price);
+CREATE INDEX shop.idx_category_name ON shop.category (name);
+CREATE INDEX shop.idx_purchase_date ON shop.purchase (date);
+CREATE INDEX shop.idx_purchase_sum ON shop.purchase (sum);
+CREATE INDEX shop.idx_consumer_full_name ON shop.consumer (full_name);
 
 
 
